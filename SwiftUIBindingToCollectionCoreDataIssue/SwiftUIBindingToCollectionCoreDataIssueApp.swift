e1c3021a1850288dcc2e25c4a7624c2922e9eb4d
Gene Bogdanovich <genebogdanovich@gmail.com>
2022-02-07 14:49:11 +0300

    Initial Commit

---
+++ SwiftUIBindingToCollectionCoreDataIssue/SwiftUIBindingToCollectionCoreDataIssueApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIBindingToCollectionCoreDataIssueApp.swift
+//  SwiftUIBindingToCollectionCoreDataIssue
+//
+//  Created by Gene Bogdanovich on 7.02.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBindingToCollectionCoreDataIssueApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
